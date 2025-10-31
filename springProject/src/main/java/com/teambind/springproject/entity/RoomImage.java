285309a75a7fc2807b7d270cf43aa4b6b565279d
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-31 15:37:18 +0900

    [REFACTOR] `RoomImage` 엔티티 개선: 접근 제어 및 연관관계 메서드 추가 springProject/src/main/java/com/teambind/springproject/entity/RoomImage.java: 양방향 연관관계 편의를 위한 assignRoom/removeRoom 메서드 추가 및 필드 접근 제어자 수정
    참고: 연관관계 관리 용이성 향상
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- springProject/src/main/java/com/teambind/springproject/entity/RoomImage.java
+++ springProject/src/main/java/com/teambind/springproject/entity/RoomImage.java
@@ -7,20 +7,29 @@ import lombok.*;
 @Entity
 @Table(name = "room_image")
 @Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class RoomImage {
-	
+
 	@Id
 	@Column(name = "image_id", length = 100)
 	private String imageId;
 	
+	@Setter(AccessLevel.PROTECTED)
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "room_id", nullable = false)
 	private RoomInfo roomInfo;
 	
 	@Column(name = "image_url", nullable = false, length = 500)
 	private String imageUrl;
+	
+	// 양방향 연관관계 편의 메서드
+	public void assignRoom(RoomInfo roomInfo) {
+		this.roomInfo = roomInfo;
+	}
+	
+	public void removeRoom() {
+		this.roomInfo = null;
+	}
 }
