189f2a1fde638dfd6b319e4810c94f5d87d1cd7f
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-31 15:36:55 +0900

    [FEAT] `RoomOptionsMapperId` 엔티티 생성 및 JPA 매핑 추가 springProject/src/main/java/com/teambind/springproject/entity/RoomOptionsMapperId.java: `@Embeddable` 클래스로 생성, `room_id`와 `keyword_id` 컬럼 정의, `equals` 및 `hashCode` 오버라이드 포함
    참고: 복합 기본키 매핑에 사용
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

---
+++ springProject/src/main/java/com/teambind/springproject/entity/RoomOptionsMapperId.java
@@ -0,0 +1,37 @@
+package com.teambind.springproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoomOptionsMapperId implements Serializable {
+	
+	@Column(name = "room_id")
+	private Long roomId;
+	
+	@Column(name = "keyword_id")
+	private Long keywordId;
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		RoomOptionsMapperId that = (RoomOptionsMapperId) o;
+		return Objects.equals(roomId, that.roomId) &&
+				Objects.equals(keywordId, that.keywordId);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(roomId, keywordId);
+	}
+}
