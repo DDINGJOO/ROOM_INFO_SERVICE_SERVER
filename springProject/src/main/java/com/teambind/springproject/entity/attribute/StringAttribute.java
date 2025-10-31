5652ab13821cab6109b775d2c4a75de561e3257f
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-31 15:23:45 +0900

    Add `StringAttribute` entity with JPA annotations for database mapping

---
+++ springProject/src/main/java/com/teambind/springproject/entity/attribute/StringAttribute.java
@@ -0,0 +1,27 @@
+package com.teambind.springproject.entity.attribute;
+
+import com.teambind.springproject.entity.RoomInfo;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "string_attribute")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "attribute_type")
+@Getter
+@Setter
+public abstract class StringAttribute {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "attribute_id")
+	private Long attributeId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "room_id", nullable = false)
+	private RoomInfo roomInfo;
+
+	@Column(name = "contents", nullable = false, length = 1000)
+	private String contents;
+}
