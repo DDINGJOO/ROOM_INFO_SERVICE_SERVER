9ca4c2fb2c3c8dc276e9bdd057a945ed51846711
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-31 15:37:06 +0900

    [REFACTOR] `StringAttribute` 및 `RoomOptionsMapper` 개선: JPA 매핑 및 관련 메서드 추가 springProject/src/main/java/com/teambind/springproject/entity/attribute/StringAttribute.java: 생성자 추가, 양방향 연관관계 메서드 정의, 최대 개수 반환 메서드 선언 springProject/src/main/java/com/teambind/springproject/entity/RoomOptionsMapper.java: 복합키 매핑 변경(EmbeddedId 사용), 팩토리 메서드 및 연관관계 편의 메서드 추가
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- springProject/src/main/java/com/teambind/springproject/entity/attribute/StringAttribute.java
+++ springProject/src/main/java/com/teambind/springproject/entity/attribute/StringAttribute.java
@@ -2,6 +2,7 @@ package com.teambind.springproject.entity.attribute;
 
 import com.teambind.springproject.entity.RoomInfo;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,18 +11,37 @@ import lombok.Setter;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "attribute_type")
 @Getter
-@Setter
 public abstract class StringAttribute {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "attribute_id")
 	private Long attributeId;
 	
+	@Setter(AccessLevel.PROTECTED)
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "room_id", nullable = false)
 	private RoomInfo roomInfo;
 	
 	@Column(name = "contents", nullable = false, length = 1000)
 	private String contents;
+	
+	protected StringAttribute() {
+	}
+	
+	protected StringAttribute(String contents) {
+		this.contents = contents;
+	}
+	
+	// 양방향 연관관계 편의 메서드 (패키지가 다르므로 public)
+	public void assignRoom(RoomInfo roomInfo) {
+		this.roomInfo = roomInfo;
+	}
+	
+	public void removeRoom() {
+		this.roomInfo = null;
+	}
+	
+	// 서브클래스에서 구현할 최대 개수 반환
+	public abstract int getMaxLimit();
 }
