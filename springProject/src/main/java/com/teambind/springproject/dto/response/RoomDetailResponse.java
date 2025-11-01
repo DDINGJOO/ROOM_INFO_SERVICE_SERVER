26936cce9826ff2890a73805dc6bb2c761bdb9a9
ddingjoo <ddingsha9@teambind.co.kr>
2025-11-01 10:32:23 +0900

    [FEAT] Room 검색을 위한 Projections 및 Repository 인터페이스 추가
    springProject/src/main/java/com/teambind/springproject/dto/projection/RoomDetailProjection.java: Room 상세 정보 조회를 위한 Projection 인터페이스 추가
    springProject/src/main/java/com/teambind/springproject/dto/projection/RoomSimpleProjection.java: Room 간단 정보 조회를 위한 Projection 인터페이스 추가
    springProject/src/main/java/com/teambind/springproject/repository/RoomQueryRepository.java: Room 검색 로직 정의를 위한 Repository 인터페이스 추가
    springProject/src/main/java/com/teambind/springproject/dto/response/RoomSimpleResponse.java: Room 간단 정보 반환을 위한 DTO 추가
    
    참고: Room 검색 서비스 구현을 위한 기준 클래스 추가.
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

---
+++ springProject/src/main/java/com/teambind/springproject/dto/response/RoomDetailResponse.java
@@ -0,0 +1,27 @@
+package com.teambind.springproject.dto.response;
+
+import com.teambind.springproject.entity.enums.Status;
+import com.teambind.springproject.entity.enums.TimeSlot;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoomDetailResponse {
+	
+	private Long roomId;
+	private String roomName;
+	private Long placeId;
+	private Status status;
+	private TimeSlot timeSlot;
+	private List<String> furtherDetails;
+	private List<String> cautionDetails;
+	private List<String> imageUrls;
+	private List<Long> keywordIds;
+}
