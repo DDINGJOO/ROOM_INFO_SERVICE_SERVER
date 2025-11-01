2b183ab8e757bc0a3d793463b4f9cab770a9a794
ddingjoo <ddingsha9@teambind.co.kr>
2025-11-01 09:48:30 +0900

    [FEAT] 방 삭제 이벤트 클래스 추가 및 방 생성 이벤트 개선 springProject/src/main/java/com/teambind/springproject/event/events/RoomCreatedEvent.java: 생성자에 roomId 초기화 로직 추가 springProject/src/main/java/com/teambind/springproject/event/events/RoomDeletedEvent.java: 방 삭제 이벤트 클래스(RoomDeletedEvent) 새로 추가
    참고: 이벤트 기반 구조 강화를 위해 추가 및 수정
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- springProject/src/main/java/com/teambind/springproject/event/events/RoomDeletedEvent.java
+++ springProject/src/main/java/com/teambind/springproject/event/events/RoomDeletedEvent.java
@@ -1,5 +1,6 @@
 package com.teambind.springproject.event.events;
 
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,11 +8,11 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
-public class RoomCreatedEvent extends Event {
+public class RoomDeletedEvent extends Event {
 	private Long roomId;
 	
-	public RoomCreatedEvent(Long roomId) {
-		super("room-created");
+	public RoomDeletedEvent(Long roomId) {
+		super("room-deleted");
 		this.roomId = roomId;
 	}
 }
