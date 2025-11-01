3258784944af4ff9f41e680321e1ecd733c1822b
ddingjoo <ddingsha9@teambind.co.kr>
2025-11-01 09:46:52 +0900

    [FEAT] 이벤트 관련 클래스 추가
    springProject/src/main/java/com/teambind/springproject/event/events/Event.java: 이벤트 기본 추상 클래스 추가
    springProject/src/main/java/com/teambind/springproject/event/events/RoomCreatedEvent.java: 방 생성 이벤트 클래스 구현
    springProject/src/main/java/com/teambind/springproject/event/events/RoomImageUpdateEvent.java: 방 이미지 업데이트 이벤트 클래스 추가
    springProject/src/main/java/com/teambind/springproject/event/events/image/ImagesChangeEventWrapper.java: 다중 이미지 변경 이벤트 래퍼 클래스 추가
    
    참고: 이벤트 기반 구조 확장을 위한 준비
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

---
+++ springProject/src/main/java/com/teambind/springproject/event/events/RoomCreatedEvent.java
@@ -0,0 +1,16 @@
+package com.teambind.springproject.event.events;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class RoomCreatedEvent extends Event {
+	private Long roomId;
+	
+	public RoomCreatedEvent(Long roomId) {
+		super("room-created");
+	}
+}
